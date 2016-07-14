0bb67988d724db9af7c2bb2e8f4ad5adbdf876cb
343_Svetlov <21arte62@gmail.com>
2016-07-14 13:07:49 +0300

    Task "Matrix Rain" completed

---
+++ Matrix_Rain/Script.js
@@ -0,0 +1,55 @@
+﻿var workWithSymbols = function () {
+    function randomSymbol(length) {
+        if (Math.random() < 0.7) {
+            return Math.floor(Math.random() * length);
+        }
+        else {
+            return " ";
+        }
+    }
+
+    return {
+        createSymbols: function (symbols) {
+            var x = 0;
+            var line = document.createElement("div");
+            for (var i = 0; i < 99; i++) {
+                var symbol = document.createElement("span");
+                symbol.textContent = symbols[randomSymbol(symbols.length)];
+                symbol.style.left = x + "em";
+                line.appendChild(symbol);
+                x += 1;
+            }
+            document.body.appendChild(line);
+            line.style.top = "0em"
+            return line;
+        }
+    };
+}();
+
+
+var MatrixRain = function () {
+    var lines = [];
+    var symbols = "1234567890qwertyuiop[]asdfghjkl;'\zxcvbnm,./+-*!@#$%^&*()";
+
+    function bias() {
+        for (var i = 0; i < lines.length; i++) {
+            lines[i].style.top = Number(lines[i].style.top.match(/\d+/)) + 1 + "em";
+        }
+        if (lines.length > 0) {
+            if (Number(lines[0].style.top.match(/\d+/)) > 44) {
+                document.body.removeChild(document.body.firstChild);
+            }
+        }
+    }
+
+    return {
+        rain: function () {
+            bias();
+            var line = workWithSymbols.createSymbols(symbols);
+            lines.push(line);
+            requestAnimationFrame(MatrixRain.rain);
+        }
+    };
+}();
+
+requestAnimationFrame(MatrixRain.rain);
